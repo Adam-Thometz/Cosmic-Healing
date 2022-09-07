184308e3be152f17554570091fef51a804e0e79b
Adam Thometz <adamthometz@gmail.com>
2022-09-06 22:24:28 -0400

    initial commit

---
+++ src/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src='' alt='' />
+      <a href='#aboutMe'>About Me</a>
+      <a href='#services'>Services</a>
+      <a href='#faq'>FAQ</a>
+    </nav>
+  );
+};
+
+export default Navbar
