0f981c41b7558dc18c1aebfb05e4f7caecd318e9
Adam Thometz <adamthometz@gmail.com>
2022-09-28 18:23:53 -0400

    added onClick

--- src/_components/button/Button.js
+++ src/_components/button/Button.js
@@ -4,10 +4,13 @@ import './Button.css';
 
 import arrow from './arrow.png';
 
-const Button = ({ primary = false,  secondary = false, children }) => {
+const Button = ({ primary = false,  secondary = false, onClick, children }) => {
   const className = `Button ${primary ? 'primary' : (secondary ? 'secondary' : 'tertiary')}`;
   const tertiary = !primary && !secondary
-  return <button className={className}>{children}{tertiary ? <img className='Button-arrow' src={arrow} alt='' /> : null}</button>;
+  return <button className={className} onClick={onClick}>
+    {children}
+    {tertiary ? <img className='Button-arrow' src={arrow} alt='' /> : null}
+  </button>;
 };
 
 export default Button;
