d56da541cfa02abc9f9ecdfe373c49072e1b789e
Adam Thometz <adamthometz@gmail.com>
2022-10-02 18:06:19 -0400

    debug/refactor

--- src/_components/section-header/SectionHeader.js
+++ src/_components/section-header/SectionHeader.js
@@ -3,12 +3,9 @@ import React from 'react';
 import './SectionHeader.css';
 
 const SectionHeader = ({ text, small = false }) => {
-  
-  return small ? (
-    <h3 className="SectionHeader">{text}</h3>
-  ) : (
-    <h2 className="SectionHeader">{text}</h2>
-  )
+  return small ?
+    <h3 className="SectionHeader">{text}</h3> :
+    <h2 className="SectionHeader">{text}</h2>;
 };
 
 export default SectionHeader;
