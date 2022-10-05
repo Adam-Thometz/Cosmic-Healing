c33fcb5b258b5b0f04c0066a8e55b0ca4d07ffb5
Adam Thometz <adamthometz@gmail.com>
2022-10-05 14:28:44 -0400

    fixed spelling error

--- src/services/serviceDescriptions.js
+++ src/services/serviceDescriptions.js
@@ -1,9 +1,9 @@
-import astronautSingle from '../_media/astronaut-single.png';
-import astronautGroup from '../_media/astronaut-group.png';
+import astronautSingle from '../_media/services/astronaut-single.png';
+import astronautGroup from '../_media/services/astronaut-group.png';
 
 const serviceDescriptions = [
   {
-    name: 'Indivudual sessions',
+    name: 'Individual sessions',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
     image: astronautSingle
   },
