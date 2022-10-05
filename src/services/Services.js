7e439c4520e35d4e246777e3223d6de06b4db783
Adam Thometz <adamthometz@gmail.com>
2022-10-05 14:44:39 -0400

    refactor

--- src/services/Services.js
+++ src/services/Services.js
@@ -5,24 +5,30 @@ import './Services.css';
 import SectionHeader from '../_components/section-header/SectionHeader';
 import SmallCaption from '../_components/small-caption/SmallCaption';
 import ServiceDescription from './service-description/ServiceDescription';
+import FAQ from './faq/FAQ';
 
 import serviceDescriptions from './serviceDescriptions';
 
 const Services = () => {
-  const services = serviceDescriptions.map((service, i) => {
-    const { name, description, image } = service;
+  const services = serviceDescriptions.map(({
+    name,
+    description,
+    image
+  }, i) => {
     return <ServiceDescription
       name={name}
       description={description}
       image={image}
       i={i}
     />
-  })
+  });
+  
   return (
     <main className='Services'>
       <SmallCaption text='DO WHAT IS BEST FOR YOU' />
       <SectionHeader text="Choose your ideal environment" />
       {services}
+      <FAQ />
     </main>
   );
 };
