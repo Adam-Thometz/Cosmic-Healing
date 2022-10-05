e807a0c9a0f0b529c178d215a1150fe74559f7ad
Adam Thometz <adamthometz@gmail.com>
2022-10-05 09:49:53 -0400

    added stars

--- src/landing-page/meet-professional/MeetProfessional.js
+++ src/landing-page/meet-professional/MeetProfessional.js
@@ -7,6 +7,7 @@ import SmallCaption from '../../_components/small-caption/SmallCaption';
 import SectionHeader from '../../_components/section-header/SectionHeader';
 
 import miriam from '../../_media/miriam2.png';
+import stars from '../../_media/stars.png';
 
 const MeetProfessional = () => {
   return (
@@ -15,6 +16,7 @@ const MeetProfessional = () => {
         <img src={miriam} alt='' />
       </aside>
       <aside className='MeetProfessional-text-wrapper'>
+        <img className='MeetProfessional-stars' src={stars} alt='' />
         <SmallCaption text="MEET THE LICENSED PROFESSIONAL" />
         <SectionHeader text="Hello, my name is Miriam!" />
         <article>I am a licensed social worker and spiritual life coach who strives to help you uncover your purpose. I use a critically conscious lens, heart-centered approach and astrology charts to align with your higher authentic self. Let's go on this journey together as we sort through and make sense of each puzzle piece to form a full picture.</article>
