2ac73ead7e2a9b469bc3cfe0e2f11b903a48568a
Adam Thometz <adamthometz@gmail.com>
2022-10-05 11:55:06 -0400

    refactor

--- src/services/service-description/ServiceDescription.test.js
+++ src/services/service-description/ServiceDescription.test.js
@@ -3,18 +3,18 @@ import { render } from "@testing-library/react";
 import ServiceDescription from './ServiceDescription';
 
 describe('ServiceDescription component', () => {
-  const serviceComponent = <ServiceDescription
+  const component = <ServiceDescription
     name='Software Testing'
     description="I love testing so much"
     image='pic of guy just testing software'
     i={0}
-  />
+  />;
   it('renders without crashing', () => {
-    render(serviceComponent);
+    render(component);
   });
 
   it('matches the snapshot', () => {
-    const { asFragment } = render(serviceComponent);
+    const { asFragment } = render(component);
     expect(asFragment()).toMatchSnapshot();
   });
 });
