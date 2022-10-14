7893e186417b4b31da54208d76be9eaf3ab6ffd2
Adam Thometz <adamthometz@gmail.com>
2022-10-14 15:58:15 -0400

    added animation

--- src/landing-page/can-help-with/CanHelpWith.js
+++ src/landing-page/can-help-with/CanHelpWith.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './CanHelpWith.css'
 
 import SmallCaption from '../../_components/small-caption/SmallCaption';
 import SectionHeader from '../../_components/section-header/SectionHeader';
 
-import hand from '../../_media/hand.png';
+import hand from '../../_media/landing-page/hand.png';
+import text from './canHelpWithText';
 
 const CanHelpWith = ({ style }) => {
-  const headerText = <>Learn how to <span className='CanHelpWith-moving-text'>improve emotional intelligence</span></>
+  const [textIdx, setTextIdx] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTextIdx(idx => idx === text.length-1 ? 0 : idx+1);
+    }, 5000);
+    return () => clearInterval(interval);
+  });
 
   return (
     <section className='CanHelpWith' style={style}>
@@ -16,8 +24,11 @@ const CanHelpWith = ({ style }) => {
         <img src={hand} alt='' />
       </aside>
       <aside className='CanHelpWith-text-wrapper'>
-        <SmallCaption text="UNDERSTAND YOURSELF AND YOUR PURPOSE" />
-        <SectionHeader text={headerText} />
+        <SmallCaption text="Understand yourself" />
+        <SectionHeader text="Learn how to" />
+        <div className='CanHelpWith-slider-wrapper'>
+          <p className='CanHelpWith-slider-text'>{text[textIdx]}</p>
+        </div>
       </aside>
     </section>
   );
---
+++ src/landing-page/can-help-with/canHelpWithText.js
@@ -0,0 +1,8 @@
+const text = [
+  'increase emotional intelligence',
+  'improve motivation and focus',
+  'strengthen relationships',
+  'boost self esteem'
+];
+
+export default text;
