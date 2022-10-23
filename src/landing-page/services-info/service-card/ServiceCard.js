9d2a04fc0df505af2f7cc66dada4bc258030f63e
Adam Thometz <adamthometz@gmail.com>
2022-10-23 13:47:26 -0400

    improved responsiveness of landing page

--- src/landing-page/services-info/service-card/ServiceCard.js
+++ src/landing-page/services-info/service-card/ServiceCard.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 import './ServiceCard.css';
 
-import Link from '../../../_components/link/Link'
-import SectionHeader from '../../../_components/section-header/SectionHeader'
+import Link from '../../../_components/link/Link';
+import SectionHeader from '../../../_components/section-header/SectionHeader';
 
 const ServiceCard = ({ service, description, link }) => {
   return (
     <div className='ServiceCard'>
       <SectionHeader small text={service} />
       <article className='ServiceCard-description'>{description}</article>
-      <Link>Learn more</Link>
+      <Link to={link} >Learn more</Link>
     </div>
-  )
-}
+  );
+};
 
-export default ServiceCard
+export default ServiceCard;
