8cca2c05ad25afeb6e48143461f65d07f05bff3f
Adam Thometz <adamthometz@gmail.com>
2022-10-26 09:47:05 -0400

    updated link

--- src/_components/link/Link.js
+++ src/_components/link/Link.js
@@ -5,7 +5,10 @@ import './Link.css';
 import arrow from './arrow.png';
 
 const Link = ({ primary = false, secondary = false, link, children }) => {
-  const btnClass = primary ? 'primary' : 'secondary'
+  const btnClass = primary ? 'primary' : (
+    secondary ? 'secondary' : 'tertiary'
+  );
+  
   return <a href={link} className={`Link ${btnClass}`}>
     {children}
     {secondary ? <img className='Link-arrow' src={arrow} alt='' /> : null}
