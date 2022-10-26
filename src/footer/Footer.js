5f06adddfbfaf0a689a7b5f6d3f3c88f4778d25d
Adam Thometz <adamthometz@gmail.com>
2022-10-26 09:48:12 -0400

    updated avaiable links

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SmallCaption from '../_components/small-caption/SmallCaption';
+import Link from '../_components/link/Link';
 
 import './Footer.css';
 
@@ -7,23 +8,15 @@ const Footer = () => {
   return (
     <footer className='Footer'>
       <section className='Footer-menu'>
-        <SmallCaption color='#FBAD7D' text="COSMIC HEALING" />
-        <a href='#'>Home</a>
-        <a href='#'>About Me</a>
-        <a href='#'>Services</a>
-        <a href='#'>FAQ</a>
-      </section>
-      <section className='Footer-menu'>
-        <SmallCaption color='#FBAD7D' text="COMMUNITY" />
-        <a href='#'>Webinars</a>
-        <a href='#'>Resources</a>
-        <a href='#'>Partnership</a>
+        <SmallCaption color='#FBAD7D' text="VESTA HEALING" />
+        <Link link='/'>Home</Link>
+        <Link link='/about-me'>About Me</Link>
+        <Link link='/services'>Services</Link>
+        <Link link='/services#faq'>FAQ</Link>
       </section>
       <section className='Footer-menu'>
         <SmallCaption color='#FBAD7D' text="COMPANY" />
-        <a href='#'>Blog</a>
-        <a href='#'>Careers</a>
-        <a href='#'>Contact us</a>
+        <Link link='/'>Contact Us</Link>
       </section>
     </footer>
   );
