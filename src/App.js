34e180a59e8e946ed660af54e19209766410361c
Adam Thometz <adamthometz@gmail.com>
2022-10-26 09:47:49 -0400

    added responsive navbar

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ import LandingPage from "./landing-page/LandingPage";
 import Services from "./services/Services";
 import Footer from './footer/Footer';
 import AboutMe from "./about-me/AboutMe";
+import NavbarResponsive from "./navbar/NavbarResponsive";
 
 function App() {
   return (
     <div className="App">
-      <Navbar style={{ "--i": 0 }} />
+      {window.innerWidth <= 750
+        ? <NavbarResponsive style={{ "--i": 0 }} />
+        : <Navbar style={{ "--i": 0 }} />
+      }
       <Routes>
         <Route index exact element={<LandingPage />} />
         <Route path="/about-me" element={<AboutMe />} />
