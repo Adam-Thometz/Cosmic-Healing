151126da32555a5a5417fb0219d306e9ac0c6e6e
Adam Thometz <adamthometz@gmail.com>
2022-10-30 17:47:56 -0400

    added onkeyup

--- src/services/faq/question/Question.js
+++ src/services/faq/question/Question.js
@@ -7,12 +7,19 @@ import './Question.css';
 import plus from '../../../_media/services/expanded.png';
 import minus from '../../../_media/services/collapsed.png';
 
-const Question = ({ id, question, answer, isShowing, handleShowing }) => {  
+const Question = ({ id, question, answer, isShowing, handleShowing }) => { 
+  const handleKeyPress = e => {
+    if (e.key === 'Enter' || e.key === 'Space') handleShowing();
+  } 
   return (
     <div className='Question' id={id} onClick={handleShowing}>
       <div className='Question-question-wrapper'>
         <SectionHeader small text={question} />
-        <img src={isShowing ? minus : plus} alt={`Pick to ${isShowing ? 'collapse' : 'expand'} question`} />
+        <img
+          role='button'
+          onKeyUp={handleKeyPress}
+          src={isShowing ? minus : plus}
+          alt={`Pick to ${isShowing ? 'collapse' : 'expand'} question`} tabIndex={0} />
       </div>
       <p className={`Question-answer${isShowing ? ' showing' : ''}`}>{answer}</p>
     </div>
