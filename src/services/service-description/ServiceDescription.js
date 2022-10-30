b5b0332f316a55183fe47430443ba863c36b3192
Adam Thometz <adamthometz@gmail.com>
2022-10-30 17:48:40 -0400

    added aria label

--- src/services/service-description/ServiceDescription.js
+++ src/services/service-description/ServiceDescription.js
@@ -6,13 +6,16 @@ import Link from '../../_components/link/Link';
 import SectionHeader from '../../_components/section-header/SectionHeader';
 
 const ServiceDescription = ({ name, description, image, i }) => {
+  const label = name === 'Group sessions' ? 'Join a group' : 'Book a free session'
   return (
     <section className={`ServiceDescription${i % 2 ? ' reverse' : ''}`}>
       <img src={image} alt='' />
       <aside className='ServiceDescription-description'>
         <SectionHeader small text={name} />
         <article>{description}</article>
-        <Link primary>{name === 'Group sessions' ? 'Join a group' : 'Book a free session'}</Link>
+        <Link primary
+          ariaLabel={`Click here to ${label}`}
+        >{label}</Link>
       </aside>
     </section>
   );
