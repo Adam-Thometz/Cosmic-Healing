d933a9fd266a471476bfb00d7109b9da917a4ef0
Adam Thometz <adamthometz@gmail.com>
2022-11-10 16:49:10 -0500

    fixed spelling error

--- src/landing-page/services-info/serviceOptions.js
+++ src/landing-page/services-info/serviceOptions.js
@@ -1,7 +1,7 @@
 const options = [
   {
     service: "Individual Sessions",
-    description: "One-to-one sessions are individualized, evidence-based therapeutic services for ages 18 and up. Services include assessment, goal setting  and treatment palnning.",
+    description: "One-to-one sessions are individualized, evidence-based therapeutic services for ages 18 and up. Services include assessment, goal setting  and treatment planning.",
     link: null
   },
   {
